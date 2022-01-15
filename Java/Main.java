e50ab34a2681880f301d8332a4fd8a5f4fb0d01f
Joe5K <45712976+Joe5K@users.noreply.github.com>
2022-01-15 15:19:45 +0100

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -3,7 +3,7 @@ package paralel_kmeans;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        var kmeans = new KMeans("iris.csv", 3, 4);
+        var kmeans = new KMeans("data/iris.csv", 3, 4);
         kmeans.work();
     }
 }
