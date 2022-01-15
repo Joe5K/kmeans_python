0a207dad4732fc6fea19c3ece03ffea486cdc177
Joe5K <jozo.krkosk4@gmail.com>
2022-01-15 15:27:35 +0100

    new code structure

--- Java/Main.java
+++
@@ -1,9 +0,0 @@
-package paralel_kmeans;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        var kmeans = new KMeans("data/iris.csv", 3, 4);
-        kmeans.work();
-    }
-}
